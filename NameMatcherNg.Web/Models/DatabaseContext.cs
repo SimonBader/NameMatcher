7aa88c97165d1092e3bf1beeab4b3f379aece6eb
Simon Bader <simonbader@yahoo.com>
2016-03-17 22:42:11 +0100

    Missing DatabaseContext.cs file added

---
+++ NameMatcherNg.Web/Models/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace NameMatcherNg.Web.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("applicationDB")
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<State> States { get; set; }
+
+        public DbSet<BabyName> Names { get; set; }
+    }
+}
